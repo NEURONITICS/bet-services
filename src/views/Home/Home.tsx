712fe7eb1659531fcb7d21f14e889ad5c6858c9a
NEURONITICS <neuronitics@protonmail.com>
2023-05-24 16:48:01 +0200

    Update Home.tsx

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -26,7 +26,7 @@ const Home = () => {
           </div>
           <div className="flex flex-col items-center justify-center">
             <div onClick={handleClick} className="py-[36px] lg:py-[48px]"><Button text={ButtonText} /></div>
-            <center><Image src="/icons/glob-icon.svg" alt="global icon" width={80} height={80} className="w-[60] h-[60] lg:w-[80px] lg:h-[80px] lg:mt-[12px]" />
+            <Image src="/icons/glob-icon.svg" alt="global icon" width={80} height={80} className="w-[60] h-[60] lg:w-[80px] lg:h-[80px] lg:mt-[12px]" />
             </div>
         </div>
       </div>
